b357568d638e59c5837e8d63531da78dc09955f1
Pawel <jankowskipawelxd@gmail.com>
2020-06-05 17:55:46 +0200

    Decentralize Services

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../common/Header'
+import ServicesItem from '../common/ServicesItem'
+import elements from '../../elements/services.json'
 
 class Services extends Component {
 	render() {
@@ -17,21 +19,9 @@ class Services extends Component {
 							<h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
 						</div>
 						<div className="row text-center">
-							<div className="col-md-4">
-								<span className="fa-stack fa-4x"><i className="fas fa-circle fa-stack-2x text-primary"></i><i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i></span>
-								<h4 className="my-3">E-Commerce</h4>
-								<p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-							</div>
-							<div className="col-md-4">
-								<span className="fa-stack fa-4x"><i className="fas fa-circle fa-stack-2x text-primary"></i><i className="fas fa-laptop fa-stack-1x fa-inverse"></i></span>
-								<h4 className="my-3">Responsive Design</h4>
-								<p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-							</div>
-							<div className="col-md-4">
-								<span className="fa-stack fa-4x"><i className="fas fa-circle fa-stack-2x text-primary"></i><i className="fas fa-lock fa-stack-1x fa-inverse"></i></span>
-								<h4 className="my-3">Web Security</h4>
-								<p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-							</div>
+							{
+								elements.map((item, id) => <ServicesItem {...item} key={id}/>	)
+							}
 						</div>
 					</div>
 				</section>
