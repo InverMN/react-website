1d245e2fba212e6acb9ff94d93e4eb50ea86352b
Pawel <jankowskipawelxd@gmail.com>
2020-06-05 16:46:16 +0200

    Add Header component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
+import Header from '../common/Header'
 
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				<header className="masthead">
-					<div className="container">
-						<div className="masthead-subheading">Welcome To Our Studio!</div>
-						<div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-						<a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-					</div>
-				</header>
-			</div>
+			<Header 
+				image="/assets/img/header-bg.jpg"
+				title="Welcome To Our Studio!"
+				subtitle="it's nice to meet you!"
+				link="/about"
+				buttonText="tell me more"
+			/>
 		)
 	}
 }
